32be48aa63616b3f85f151c4b355a6982c4aa937
Alexander Heldt <heldt@skovik.com>
2013-05-11 12:35:24 +0200

    Divide height by 2

--- ascii_aalib.py
+++ ascii_aalib.py
@@ -2,7 +2,7 @@ import aalib
 
 def convert(image):
     width, height = image.size
-    screen = aalib.AsciiScreen(width=width, height=height)
+    screen = aalib.AsciiScreen(width=width, height=height/2)
     i = image.convert('L')
     screen.put_image((0, 0), i)
     return screen.render()
