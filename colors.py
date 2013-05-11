8b664cb9f243d6ecb8b59920e71bd2f93211a9aa
Alexander Blom <me@alexanderblom.se>
2013-05-11 17:42:22 +0200

    Reduce color depth to preserve original colors

--- colors.py
+++ colors.py
@@ -9,17 +9,17 @@ _colors = {}
 def init():
     pair = 0
 
-    for r, g, b in product(range(0b111 + 1), range(0b111 + 1), range(0b11 + 1)):
-        if (r, g, b) == (0b111, 0b111, 0b11):
-            break
+    for r, g, b in product(range(0b111 + 1), range(0b11 + 1), range(0b11 + 1)):
+        #if (r, g, b) == (0b111, 0b111, 0b11):
+        #    break
 
         pair += 1
-        curses.init_color(pair - 1, int(r / float(0b111) * 1000), int(g / float(0b111) * 1000), int(b / float(0b11) * 1000))
-        curses.init_pair(pair, pair - 1, 0)
+        curses.init_color(pair + 10, int(r / float(0b111) * 1000), int(g / float(0b11) * 1000), int(b / float(0b11) * 1000))
+        curses.init_pair(pair, pair + 10, 0)
 
         _colors[(r, g, b)] = pair
 
     return
 
 def pair(r, g, b):
-    return _colors.get((r >> 5, g >> 5, b >> 6), 0)
+    return _colors.get((r >> 5, g >> 6, b >> 6), 0)
